3076952a3336b76ffd8af0016ae7b6da5cc85c28
Oliver Apelqvist Eriksson <oliape060414@student.jenseneducation.se>
2024-03-07 12:05:59 +0100

    Add project files.

---
+++ 2024/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024
+{
+    public class Hund
+    {
+        public int ben { get; set; }
+        private string päls;
+
+        public void setPäls(string p)
+        {
+            this.päls = p;
+        }
+        public string getPäls()
+        {
+            return this.päls;
+        }
+    }
+
+    public class franchise
+    {
+        private string FranchiseName;
+        private string FranchiseGenre;
+    }
+    public class karakters:franchise
+    {
+        public int age { get; set; }
+        private string CharacterName;
+        private string description;
+        public string gender;
+        private bool alive { get; set; }
+    }
+    public class main_character:karakters
+    {
+        private bool plot_armour;
+        private int teammates;
+
+
+        
+        public void setTeam(int t)
+        {
+            teammates = t;
+        }
+        public int getTeam()
+        {
+            return teammates;
+        }
+        public main_character() {
+            Console.WriteLine("Main character was not filled");
+        }
+        public main_character(string FranchiseName, string FranchiseGenre, string CharacterName, int age, string description, string gender, bool alive, bool plot_armour, int teammates)
+        {
+            FranchiseName = FranchiseName;
+            FranchiseGenre = FranchiseGenre;
+            CharacterName = CharacterName;
+            description = description;
+            gender = gender;
+            alive = alive;
+            plot_armour = plot_armour;
+            teammates = teammates;
+            age = age;
+            Console.WriteLine("MC setup complete");
+        }
+    }
+}
---
+++ 2024/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hund hund1 = new Hund();
+            hund1.setPäls("grön");
+            Console.WriteLine(hund1.getPäls());
+            hund1.ben = 4;
+            Console.WriteLine(hund1.ben);
+
+            main_character mc = new main_character("", "", "", 18, "", "", true, true, 5);
+
+
+
+        }
+    }
+}
